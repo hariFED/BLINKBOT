5dc33425d1567301e8ee148479cd7111b58d7b0a
AvhiMaz <avhimazumder5@outlook.com>
2025-01-19 10:38:47 +0530

    smoll fix hehehe

--- src/app/api/memecoin/route.ts
+++ src/app/api/memecoin/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 const DEXSCREENER_API = process.env.PROVIDER_URL;
 
 export async function GET(request: Request) {
-
   const { searchParams } = new URL(request.url);
 
   const contractAddress = searchParams.get("ca");
@@ -11,59 +10,52 @@ export async function GET(request: Request) {
   const chainId = "solana";
 
   if (!contractAddress) {
-
     return NextResponse.json(
       { error: "Contract address (ca) is required." },
-      { status: 400 },
+      { status: 400 }
     );
-
   }
 
   try {
-
     const response = await fetch(
-
-      `${DEXSCREENER_API}/${chainId}/${contractAddress}`,
-
+      `${DEXSCREENER_API}/${chainId}/${contractAddress}`
     );
 
     if (!response.ok) {
-      
       return NextResponse.json(
-      
         { error: `Failed to fetch token data: ${response.statusText}` },
-      
-        { status: response.status },
-        
-      );
 
+        { status: response.status }
+      );
     }
 
-    const data = await response.json();
+    const data = (await response.json())[0];
+
+    if (!data) {
+      return NextResponse.json(
+        { error: "Failed to fetch token data." },
+        { status: 500 }
+      );
+    }
 
     const result = {
-
-      name: data.basetoken.name,
-      symbol: data.basetoken.symbol,
+      name: data.baseToken.name,
+      symbol: data.baseToken.symbol,
+      address: data.baseToken.address,
       marketCap: data.marketCap,
       price: data.priceUsd,
       circulatingSupply: data.marketCap / data.priceUsd,
       totalSupply: data.fdv / data.priceUsd,
-
     };
 
     return NextResponse.json(result, { status: 200 });
-
   } catch (error) {
-
     console.error("Error fetching meme coin info:", error);
 
     return NextResponse.json(
-
       { error: "Internal server error. Please try again later." },
 
-      { status: 500 },
-      
+      { status: 500 }
     );
   }
 }
