705bf41faa00cca82f81178c39b636d80541ba8f
shivendram16053 <shivendramishra.sm93@gmail.com>
2025-01-21 00:50:04 +0530

    phantom wallet added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // app/page.tsx
 "use client";
 
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import ConnectButton from "./components/AppWalletProvider";
 import BlinkProvider from "./components/BlinkProvider";
 
@@ -8,7 +9,7 @@ export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <div>
-        <ConnectButton />
+        <WalletMultiButton/>
       </div>
       <div>
         <BlinkProvider />
